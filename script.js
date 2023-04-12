2c761cad3ae9ea54eceabacf7941edd611515821
copestakes <davecopestakes@gmail.com>
2023-04-11 20:56:44 -0400

    updated script

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ const date = new Date();
 const year = date.getFullYear();
 let copyRight = document.querySelector('.copyright');
 copyRight.innerHTML = `Copyright ${year} David Copestakes`;
-console.log(year);
 
 const navbar = document.querySelector('.navigation');
 const navHeight = navbar.getBoundingClientRect().height;
-console.log(navHeight);
