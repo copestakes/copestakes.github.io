308ff9e79174b199317ffcbf4bf7ace654a4ca8b
copestakes <davecopestakes@gmail.com>
2023-04-15 00:48:57 -0400

    updated navigation

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
+const input = document.querySelector('#check');
+const nav = document.querySelector('nav ul');
+const anchors = document.querySelectorAll('a');
+
 function checkAll() {
-  const input = document.querySelector('#check');
   input.checked = false;
 }
 window.onload = function () {
   window.addEventListener('resize', checkAll, false);
+  nav.addEventListener('click', checkAll, false);
 };
 
-window.addEventListener('click', function () {
-  var checkbox = document.querySelector('#check');
-  checkbox.checked = !checkbox.checked;
+anchors.forEach((e) => {
+  e.addEventListener('click', checkAll, false);
 });
+
+// window.addEventListener('click', function () {
+//   const checkbox = document.querySelector('#check');
+//   checkbox.checked = !checkbox.checked;
+// });
+
+// const menu_btn = document.querySelector('#check');
+// const menu_btn_ham = document.querySelector('.hamburger');
+// const hamburger_menu = document.querySelector('.nav');
+// const anchors = document.querySelectorAll('a');
+
+// function openMenu() {
+//   menu_btn_ham.classList.toggle('is-active');
+//   hamburger_menu.classList.toggle('is-active');
+// }
+
+// menu_btn.addEventListener('click', openMenu);
+
+// anchors.forEach((e) => {
+//   e.addEventListener('click', openMenu);
+// });
 
 const date = new Date();
 const year = date.getFullYear();
